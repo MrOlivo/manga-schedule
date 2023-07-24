8873f5e3ecdf2d94164fa27f9d3915e44f2f2dc3
Edwin Olivo Garcia <17003685@uan.edu.mx>
2023-07-23 22:12:11 -0700

    create a section for "OpenIn" actions

--- src/components/MangaListItem.tsx
+++ src/components/MangaListItem.tsx
@@ -16,35 +16,37 @@ export default function MangaListItem({ manga }: Props) {
       actions={
         <ActionPanel>
           <Action.Push title="Details" target={<DetailsView manga={manga} />} />
-          {manga.editorial.includes("Panini") && (
+          <ActionPanel.Section title="Open in...">
+            {manga.editorial.includes("Panini") && (
+              <Action.OpenInBrowser
+                title="Panini"
+                url={`https://tiendapanini.com.mx/catalogsearch/result/?q=${manga.name.replaceAll(" ", "+")}`}
+                shortcut={{ modifiers: ["cmd"], key: "enter" }}
+              />
+            )}
+            {manga.editorial.includes("Kamite") && (
+              <Action.OpenInBrowser
+                title="Kamite"
+                url={`https://kamite.com.mx/buscar?controller=search&poscats=0&s=${manga.name.replaceAll(" ", "+")}`}
+                shortcut={{ modifiers: ["cmd"], key: "enter" }}
+              />
+            )}
+            {manga.editorial.includes("Penguin") && (
+              <Action.OpenInBrowser
+                title="Penguin Random House"
+                url={`https://www.penguinlibros.com/mx/?mot_q=${manga.name.replaceAll(" ", "-")}#`}
+                shortcut={{ modifiers: ["cmd"], key: "enter" }}
+              />
+            )}
             <Action.OpenInBrowser
-              title="Open in Panini"
-              url={`https://tiendapanini.com.mx/catalogsearch/result/?q=${manga.name.replaceAll(" ", "+")}`}
-              shortcut={{ modifiers: ["cmd"], key: "1" }}
+              title="Sanborns"
+              url={`https://www.sanborns.com.mx/resultados?query=${manga.name.replaceAll(" ", "%2520")}`}
             />
-          )}
-          {manga.editorial.includes("Kamite") && (
             <Action.OpenInBrowser
-              title="Open in Kamite"
-              url={`https://kamite.com.mx/buscar?controller=search&poscats=0&s=${manga.name.replaceAll(" ", "+")}`}
-              shortcut={{ modifiers: ["cmd"], key: "1" }}
+              title="Buscalibre"
+              url={`https://www.buscalibre.com.mx/libros/search?q=${manga.name.replaceAll(" ", "+")}`}
             />
-          )}
-          {manga.editorial.includes("Penguin") && (
-            <Action.OpenInBrowser
-              title="Open in Penguin Random House"
-              url={`https://www.penguinlibros.com/mx/?mot_q=${manga.name.replaceAll(" ", "-")}#`}
-              shortcut={{ modifiers: ["cmd"], key: "1" }}
-            />
-          )}
-          <Action.OpenInBrowser
-            title="Open in Sanborns"
-            url={`https://www.sanborns.com.mx/resultados?query=${manga.name.replaceAll(" ", "%2520")}`}
-          />
-          <Action.OpenInBrowser
-            title="Open in Buscalibre"
-            url={`https://www.buscalibre.com.mx/libros/search?q=${manga.name.replaceAll(" ", "+")}`}
-          />
+          </ActionPanel.Section>
         </ActionPanel>
       }
     />
