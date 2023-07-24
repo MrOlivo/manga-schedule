d54580e2a9119099c64a2cc3a7d7ecd4ef6a72bf
Edwin Olivo Garcia <17003685@uan.edu.mx>
2023-07-24 12:30:14 -0700

    renamed the interfaces [4bdecfc7]

--- src/utils/scrapper.ts
+++ src/utils/scrapper.ts
@@ -1,14 +1,14 @@
 import * as cheerio from 'cheerio';
-import { ICollection, IManga } from '../types'
+import { Collection, Manga } from '../types'
 
-export async function scrapeManga(body: string): Promise<IManga[]> {
+export async function scrapeManga(body: string): Promise<Manga[]> {
   try {
     const $ = cheerio.load(body);
-    const Shelf: IManga[] = [];
+    const Shelf: Manga[] = [];
     // Select by the 'comic' class and use the attr method to only grab the desired content
     // This work for both Calendario and Colección
     $('div.comic').map((_idx, el) => {
-      const manga: IManga = {
+      const manga: Manga = {
         name: $(el).attr('data-c')?.split(" ").slice(0, -1).join(" ") || "",
         volume: $(el).attr('data-no') || "",
         price: $(el).attr('data-p') || "",
@@ -29,10 +29,10 @@ export async function scrapeManga(body: string): Promise<IManga[]> {
 export async function scrapeCollections(body: string) {
   try {
     const $ = cheerio.load(body);
-    const Shelf: ICollection[] = [];
+    const Shelf: Collection[] = [];
 
     $('div.colex').map((_idx, el) => {
-      const collection: ICollection = {
+      const collection: Collection = {
         name: $(el).find('div.n').text() || "",
         url: $(el).find('a').attr('href') || "",
         frontImageUrl: $(el).find('img').attr('src') || "",
