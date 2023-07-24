14f95d9ccd83c93e3d5be0d475fa57c8dcfb0f89
Edwin Olivo Garcia <17003685@uan.edu.mx>
2023-07-24 12:28:19 -0700

    function that prepares the text for url parameter

--- src/components/MangaListItem.tsx
+++ src/components/MangaListItem.tsx
@@ -1,9 +1,10 @@
 import { Action, ActionPanel, Icon, Image, List } from "@raycast/api";
 import DetailsView from "./DetailsView";
-import { IManga } from "../types";
+import { Manga } from "../types";
+import { getNormalizedParameter } from "../utils/normalize";
 
 interface Props {
-  manga: IManga;
+  manga: Manga;
 }
 
 export default function MangaListItem({ manga }: Props) {
@@ -20,31 +21,31 @@ export default function MangaListItem({ manga }: Props) {
             {manga.editorial.includes("Panini") && (
               <Action.OpenInBrowser
                 title="Panini"
-                url={`https://tiendapanini.com.mx/catalogsearch/result/?q=${manga.name.replaceAll(" ", "+")}`}
+                url={`https://tiendapanini.com.mx/catalogsearch/result/?q=${getNormalizedParameter(manga.name, "+")}`}
                 shortcut={{ modifiers: ["cmd"], key: "enter" }}
               />
             )}
             {manga.editorial.includes("Kamite") && (
               <Action.OpenInBrowser
                 title="Kamite"
-                url={`https://kamite.com.mx/buscar?controller=search&poscats=0&s=${manga.name.replaceAll(" ", "+")}`}
+                url={`https://kamite.com.mx/buscar?controller=search&poscats=0&s=${getNormalizedParameter(manga.name, "+")}`}
                 shortcut={{ modifiers: ["cmd"], key: "enter" }}
               />
             )}
             {manga.editorial.includes("Penguin") && (
               <Action.OpenInBrowser
                 title="Penguin Random House"
-                url={`https://www.penguinlibros.com/mx/?mot_q=${manga.name.replaceAll(" ", "-")}#`}
+                url={`https://www.penguinlibros.com/mx/?mot_q=${getNormalizedParameter(manga.name, "-")}#`}
                 shortcut={{ modifiers: ["cmd"], key: "enter" }}
               />
             )}
             <Action.OpenInBrowser
               title="Sanborns"
-              url={`https://www.sanborns.com.mx/resultados?query=${manga.name.replaceAll(" ", "%2520")}`}
+              url={`https://www.sanborns.com.mx/resultados?query=${getNormalizedParameter(manga.name, "%2520")}`}
             />
             <Action.OpenInBrowser
               title="Buscalibre"
-              url={`https://www.buscalibre.com.mx/libros/search?q=${manga.name.replaceAll(" ", "+")}`}
+              url={`https://www.buscalibre.com.mx/libros/search?q=${getNormalizedParameter(manga.name, "+")}`}
             />
           </ActionPanel.Section>
         </ActionPanel>
