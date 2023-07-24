9876d1d9a702d6c89d5f368e7fa369599f7fe918
Edwin Olivo Garcia <17003685@uan.edu.mx>
2023-07-23 21:59:08 -0700

    removed shortcuts related to the retailer's webs

--- src/components/MangaListItem.tsx
+++ src/components/MangaListItem.tsx
@@ -20,32 +20,30 @@ export default function MangaListItem({ manga }: Props) {
             <Action.OpenInBrowser
               title="Open in Panini"
               url={`https://tiendapanini.com.mx/catalogsearch/result/?q=${manga.name.replaceAll(" ", "+")}`}
-              shortcut={{ modifiers: ["cmd"], key: "enter" }}
+              shortcut={{ modifiers: ["cmd"], key: "1" }}
             />
           )}
           {manga.editorial.includes("Kamite") && (
             <Action.OpenInBrowser
               title="Open in Kamite"
               url={`https://kamite.com.mx/buscar?controller=search&poscats=0&s=${manga.name.replaceAll(" ", "+")}`}
-              shortcut={{ modifiers: ["cmd"], key: "enter" }}
+              shortcut={{ modifiers: ["cmd"], key: "1" }}
             />
           )}
           {manga.editorial.includes("Penguin") && (
             <Action.OpenInBrowser
               title="Open in Penguin Random House"
               url={`https://www.penguinlibros.com/mx/?mot_q=${manga.name.replaceAll(" ", "-")}#`}
-              shortcut={{ modifiers: ["cmd"], key: "enter" }}
+              shortcut={{ modifiers: ["cmd"], key: "1" }}
             />
           )}
           <Action.OpenInBrowser
             title="Open in Sanborns"
             url={`https://www.sanborns.com.mx/resultados?query=${manga.name.replaceAll(" ", "%2520")}`}
-            shortcut={{ modifiers: ["cmd", "shift"], key: "1" }}
           />
           <Action.OpenInBrowser
             title="Open in Buscalibre"
             url={`https://www.buscalibre.com.mx/libros/search?q=${manga.name.replaceAll(" ", "+")}`}
-            shortcut={{ modifiers: ["cmd", "shift"], key: "2" }}
           />
         </ActionPanel>
       }
