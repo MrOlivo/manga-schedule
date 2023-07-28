c50942470713c3779e637ffe4d0419e8ca47fbc2
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2023-07-28 11:02:46 -0700

    refactor: CurrentMonthPublications component
    Created usePagination function to handle data pagination.
    Extracted handlePageChange function to handle page changes from the dropdown.
    Extracted renderDropdownItems function to generate dropdown items.
    Destructured props for better readability and to avoid redundant props prefix.

--- src/commands/BrowseCollections.tsx
+++ src/commands/BrowseCollections.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import { Collection } from "../types";
 import { scrapeCollections } from "../utils/scrapper";
 import CollectionGridItem from "../components/CollectionGridItem";
+import usePagination from "../hooks/usePagination";
 
 export default function CurrentMonthPublications() {
   const [collectionList, setCollectionList] = useState<Collection[]>([]);
@@ -11,22 +12,36 @@ export default function CurrentMonthPublications() {
   const { isLoading, data } = useFetch("https://miscomics.com.mx/manga", {
     keepPreviousData: true,
   });
+  const { paginatedData, totalPages, setPage } = usePagination(collectionList);
 
   useEffect(() => {
     scrapeCollections(String(data) || "").then((result) => setCollectionList(result));
   }, [data]);
+
+  const handlePageChange = (newValue: string) => setPage(Number(newValue) || 1);
+
+  const renderDropdownItems = () => {
+    return Array(totalPages)
+      .fill(null)
+      .map((_, idx) => <Grid.Dropdown.Item title={`Page ${idx + 1}`} value={String(idx + 1)} key={idx} />);
+  };
 
   return (
     <Grid
       navigationTitle={`${collectionList.length} Manga collections were found`}
       isLoading={isLoading}
       columns={5}
-      inset={Grid.Inset.Small}
+      inset={Grid.Inset.Zero}
       searchText={searchText}
       onSearchTextChange={setSearchText}
       filtering={true}
+      searchBarAccessory={
+        <Grid.Dropdown tooltip="Buscar" onChange={handlePageChange}>
+          {renderDropdownItems()}
+        </Grid.Dropdown>
+      }
     >
-      {collectionList.map((collection, idx) => (
+      {paginatedData.map((collection, idx) => (
         <CollectionGridItem key={idx + collection.name} collection={collection} />
       ))}
     </Grid>
